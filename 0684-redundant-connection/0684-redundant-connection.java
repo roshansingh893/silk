1f4d20417cd97e9a0ced49505e34f14a28aabf8c
Roshan Kumar Singh <roshankumar65167@gmail.com>
2026-08-16 15:46:34 +0530

    Time: 1 ms (91.81%), Space: 45.1 MB (53.49%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,43 +1,65 @@
 class Solution {
+    class DSU {
+
+    int[] parent;
+    int[] size;
+
+    DSU(int n) {
+
+        parent = new int[n + 1];
+        size = new int[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int find(int x) {
+
+        if(parent[x] == x)
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    boolean union(int a, int b) {
+
+        int pa = find(a);
+        int pb = find(b);
+
+        if(pa == pb)
+            return false;
+
+        if(size[pa] < size[pb]) {
+            parent[pa] = pb;
+            size[pb] += size[pa];
+        } else {
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        }
+
+        return true;
+    }
+}
     public int[] findRedundantConnection(int[][] edges) {
-        //boolean[] vis = new boolean[edges.length];
+
         int n = edges.length;
 
-List<List<Integer>> graph = new ArrayList<>();
+        DSU dsu = new DSU(n);
 
-for (int i = 0; i <= n; i++) {
-    graph.add(new ArrayList<>());
-}
-        for(int[] e : edges){
-            int u = e[0];
-            int v = e[1];
-            boolean[] vis = new boolean[edges.length + 1];
-            if(dfs(u , v ,vis , graph )){
-                return e;
+        for(int[] edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            if(dsu.find(u) == dsu.find(v)) {
+                return edge;
             }
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+
+            dsu.union(u, v);
         }
 
-        return new int[] {- 1 , - 1};
+        return new int[0];
     }
-
-   public boolean dfs(int node, int target, boolean[] vis , List<List<Integer>> graph){
-
-    if(node == target)
-        return true;
-
-    vis[node] = true;
-
-    for(int nei : graph.get(node)){
-
-        if(!vis[nei]){
-
-            if(dfs(nei, target, vis , graph))
-                return true;
-        }
-    }
-
-    return false;
-}
 }
