7aa8f9a262c701d213f29dd8c10348e4a33c5d3d
Roshan Kumar Singh <roshankumar65167@gmail.com>
2026-08-16 15:16:08 +0530

    Time: 1 ms (86.48%), Space: 47.3 MB (43.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,74 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int count = 0;
-        boolean[] vis = new boolean[n];
-        for(int i =0; i<n; i++){
-            if(!vis[i]){
-                count++;
-                dfs(isConnected , i , vis);
-            }
+
+
+class DSU {
+
+    int[] parent;
+    int[] size;
+
+    DSU(int n){
+
+        parent = new int[n];
+        size = new int[n];
+
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
-        return count;
     }
 
-    public void dfs(int[][] isConnected , int curr , boolean[] vis){
-        vis[curr] = true;
+    int find(int x){
 
-        for(int i = 0; i < isConnected.length; i++){
-            if(isConnected[curr][i] == 1 && !vis[i]){
-                dfs(isConnected , i , vis);
+        if(parent[x]==x)
+            return x;
+
+        return parent[x]=find(parent[x]);
+    }
+
+    boolean union(int a,int b){
+
+        int pa=find(a);
+        int pb=find(b);
+
+        if(pa==pb)
+            return false;
+
+        if(size[pa]<size[pb]){
+
+            parent[pa]=pb;
+            size[pb]+=size[pa];
+        }
+        else{
+
+            parent[pb]=pa;
+            size[pa]+=size[pb];
+        }
+
+        return true;
+    }
+}
+
+    public int findCircleNum(int[][] isConnected) {
+
+        int n = isConnected.length;
+
+        DSU dsu = new DSU(n);
+
+        int provinces = n;
+
+        for(int i=0;i<n;i++){
+
+            for(int j=i+1;j<n;j++){
+
+                if(isConnected[i][j]==1){
+
+                    if(dsu.union(i,j)){
+                        provinces--;
+                    }
+                }
             }
         }
 
+        return provinces;
     }
 }
