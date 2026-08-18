299d9dd743b66c4c3fcc5132048f3c63c3a8b5f2
Roshan Kumar Singh <roshankumar65167@gmail.com>
2026-08-18 13:08:58 +0530

    Time: 335 ms (44.73%), Space: 98.3 MB (29.04%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,85 @@
+class Solution {
+
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); 
+            }
+            return parent[x];
+        }
+
+        boolean union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+
+            if (pa == pb) {
+                return false;
+            }
+
+            if (rank[pa] < rank[pb]) {
+                parent[pa] = pb;
+            } else if (rank[pb] < rank[pa]) {
+                parent[pb] = pa;
+            } else {
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+
+            return true;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+
+        List<int[]> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int cost =
+                        Math.abs(points[i][0] - points[j][0]) +
+                        Math.abs(points[i][1] - points[j][1]);
+
+                edges.add(new int[]{cost, i, j});
+            }
+        }
+        edges.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+        DSU dsu = new DSU(n);
+
+        int mstCost = 0;
+        int edgesUsed = 0;
+
+        for (int[] edge : edges) {
+
+            int cost = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (dsu.union(u, v)) {
+
+                mstCost += cost;
+                edgesUsed++;
+
+                if (edgesUsed == n - 1) {
+                    break;
+                }
+            }
+        }
+
+        return mstCost;
+    }
+}
