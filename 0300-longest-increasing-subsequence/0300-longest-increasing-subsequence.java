9c2b12159f6503e5d0ad574b260ad1f2a376f6dc
Roshan Kumar Singh <roshankumar65167@gmail.com>
2026-09-13 10:41:54 +0530

    Time: 71 ms (30.03%), Space: 46.1 MB (57.4%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,30 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-         // -> this is patience sorting approach 
-         // tails integer 
-        List<Integer> tails = new ArrayList<>();
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp , -1);
 
-        for (int num : nums) {
+        int max = 0;
 
-            int left = 0;
-            int right = tails.size();
+        for(int i = 0; i < n; i++){
+            max = Math.max(max , solve(nums , dp , n , i));
+        }
 
-            while (left < right) {
-                int mid = left + (right - left) / 2;
+        return  max;
+    }
 
-                if (tails.get(mid) < num) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
 
-            if (left == tails.size()) {
-                tails.add(num);
-            } else {
-                tails.set(left, num);
+    public int solve(int[] nums , int[] dp , int n , int i){
+        if(dp[i] != -1){
+            return dp[i];
+        }
+          int count = 1;
+        for(int j = i; j < n; j++){
+            if(nums[j] > nums[i]){
+                count = Math.max(count , 1 + solve(nums , dp , n , j));
             }
         }
 
-        return tails.size();
+        return   dp[i] =  count;
     }
 }
